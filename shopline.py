d88203a1c5341ee7c4d21c373f0238c9f8b5a9f2
Jason Sung <07711.Jason.Sung@ict888.net>
2025-05-02 17:46:59 +0800

    cloudbuild logging option

--- shopline.py
+++ shopline.py
@@ -2,6 +2,7 @@ import requests
 import json
 from loguru import logger
 
+
 class ShopLine:
 
     def __init__(self, order_number):
@@ -17,11 +18,11 @@ class ShopLine:
         self.order_detail = {}
         self.order_id = ""
         self.setup()
-    
+
     def setup(self):
         self.order_detail = self.query_order(self.order_number)
         self.order_id = self.order_detail["items"][0]["id"]
-    
+
     def response_handler(self, response):
         if response.status_code == 200:
             try:
@@ -35,47 +36,42 @@ class ShopLine:
         else:
             logger.error(f"{response.status_code} : {response.text}")
 
-    
     def get_token_info(self):
         url = f"{self.domain}/v1/token/info"
         response = requests.get(url=url, headers=self.header)
         return self.response_handler(response)
-    
+
     def get_order(self):
         url = f"{self.domain}/v1/orders/:{self.order_id}"
         response = requests.get(url=url, headers=self.header)
         if response.status_code == 410:
             logger.error(f"{self.order_id} 此 Order 封存")
         elif response.status_code == 404:
-            logger.error(f"{self.order_id} 此 Order 不存在") 
+            logger.error(f"{self.order_id} 此 Order 不存在")
         else:
-            return self.response_handler(response)   
-    
+            return self.response_handler(response)
+
     def update_delivery_status(self, status, notify):
-        #pending, shipping, shipped, arrived, collected, returned, returning
+        # pending, shipping, shipped, arrived, collected, returned, returning
         url = f"{self.domain}/v1/orders/{self.order_id}/order_delivery_status"
-        payload = {"id": self.order_id, "status":status, "mail_notify":notify}
+        payload = {"id": self.order_id, "status": status, "mail_notify": notify}
         response = requests.put(url=url, headers=self.header, data=payload)
         return self.response_handler(response)
 
     def update_order_status(self, status):
-        #pending, confirmed, completed, cancelled
+        # pending, confirmed, completed, cancelled
         url = f"{self.domain}/v1/orders/{self.order_id}/status"
-        payload = {
-            "id":self.order_id,
-            "mail_notify": False,
-            "status":status
-        }
+        payload = {"id": self.order_id, "mail_notify": False, "status": status}
         response = requests.patch(url=url, headers=self.header, data=payload)
         return self.response_handler(response)
 
     def update_order(self):
         url = f"{self.domain}/v1/orders/{self.order_id}/status"
-        payload = {"mail_notify": False,"status":"completed"}
+        payload = {"mail_notify": False, "status": "completed"}
         response = requests.patch(url=url, headers=self.header, data=payload)
         return self.response_handler(response)
-    
-    def search_order(self, specific_conditions:dict):
+
+    def search_order(self, specific_conditions: dict):
         """
         ::param specific_conditions: dict
         specific_conditions = {
@@ -95,22 +91,22 @@ class ShopLine:
         for key, value in specific_conditions.items():
             if value:
                 if isinstance(value, list):
-                    value = ','.join(value)
+                    value = ",".join(value)
                 query_params.append(f"{key}={value}")
         query_string = "&".join(query_params)
         full_url = f"{url}?{query_string}"
         print(full_url)
         response = requests.get(url=full_url, headers=self.header)
         return self.response_handler(response)
-    
+
     def get_order_delivery(self):
         url = f"{self.domain}/v1/order_deliveries/{self.order_id}"
         response = requests.get(url=url, headers=self.header)
         return self.response_handler(response)
-    
+
     def query_order(self, query):
-        return self.search_order({"query":query})
-    
+        return self.search_order({"query": query})
+
     def update_delivery_options(self):
         url = f"{self.domain}/v1/delivery_options/{self.order_id}/stores_info"
 
